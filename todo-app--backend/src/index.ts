e415c1b05b86ba0fb4aec43876d6fdd6a57afeaf
DE-Uehara <r.uehara@defide-ix.com>
2024-10-08 12:38:33 +0900

    supabase接続

--- todo-app--backend/src/index.ts
+++ todo-app--backend/src/index.ts
@@ -1,9 +1,16 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { users } from "./api/user/route";
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("/users", users);
 
-export default app
+const port = 8080
+
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
--- todo-app--frontend/src/app/page.tsx
+++ todo-app--frontend/src/app/page.tsx
@@ -1,10 +1,22 @@
+interface DATATYPE {
+  id: string;
+  name: string;
+}
+
 export default async function Home() {
-  const res = await fetch("http://localhost:8000/");
-  const data = await res.json(); // レスポンスをJSONとしてパース
+  const res = await fetch("http://localhost:8080/users");
+  const data: Array<DATATYPE> = await res.json(); // レスポンスをJSONとしてパース
 
   return (
     <div>
-      <p>{data}</p>
+      {data.map((user) => {
+        return (
+          <>
+            <div>id: {user.id}</div>
+            <div>name: {user.name}</div>
+          </>
+        );
+      })}
     </div>
   );
 }
