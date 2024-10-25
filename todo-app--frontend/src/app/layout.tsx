1c2507c72bb053d4f4127fbc1f8a209897264a46
DE-Uehara <r.uehara@defide-ix.com>
2024-10-25 11:55:51 +0900

    デザイン

--- todo-app--frontend/src/app/layout.tsx
+++ todo-app--frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./task/task-page.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- todo-app--frontend/src/app/task/page.tsx
+++ todo-app--frontend/src/app/task/page.tsx
@@ -1,19 +1,40 @@
 import React from "react";
 
 const page = async () => {
-  const responsData = await fetch("http://localhost:8080/task");
-  const tasks = await responsData.json();
+  const responsData = await fetch("http://localhost:8080/task/getTask");
+  const tasks: TaskArg[] = await responsData.json();
   return (
-    <div className="task">
-      {tasks.map((task) => {
-        return (
-          <div className="task" key={task.id}>
-            {task.id}
-          </div>
-        );
-      })}
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>task</th>
+          <th>start</th>
+          <th>end</th>
+          <th>memo</th>
+          <th>ticketID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => (
+          <tr key={task.id}>
+            <td>{task.taskName}</td>
+            <td>{task.start}</td>
+            <td>{task.end}</td>
+            <td>{task.comments}</td>
+            <td>{task.id}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 export default page;
+
+export interface TaskArg {
+  id: number;
+  start: string;
+  end: string;
+  comments: string;
+  taskName: string;
+}
