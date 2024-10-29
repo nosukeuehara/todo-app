01fcd0e5c26545c759afa4e454392998fe5434ac
DE-Uehara <r.uehara@defide-ix.com>
2024-10-29 15:49:48 +0900

    フロントのつくりかえ

--- todo-app--frontend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      Hello
-      <p>nosuke</p>
-    </div>
-  );
-}
--- todo-app--frontend/src/app/task/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "./task-page.css";
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <div className="wrapper">{children}</div>;
-}
